7e4a92b422a1682ad57a1bce3d134beb74597cce
echudov <echudov@gmail.com>
2020-01-12 14:14:37 -0600

    minor dependency changes
    changed article parsing api from boilerpy3 to newspaper
    
    next steps:
    1. source necessary political sentiment data
    2. organize that shid
    3. create infrastructure to use google nlp sent analysis (fairly straightforward once data structures are determined

---
+++ text_analysis_functions.py
@@ -0,0 +1,13 @@
+import argparse
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+def analyze(filename):
+    client = language.LanguageServiceClient()
+
+    with open(filename, 'r') as articleFile:
+        content = articleFile.read()
+
+    document = types.Document(content=content, type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
